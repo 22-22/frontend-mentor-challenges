1b5fe3a9836f77524901f5f73c58bd0bb1bc5b20
Yuliya Kogol <kogol.julia@gmail.com>
2025-02-26 11:47:43 +0100

    Move loopstudio files to a separate branch

--- loopstudios-landing-page/app/ui/common/Logo.tsx
+++
@@ -1,7 +0,0 @@
-import { alata } from "../../fonts";
-
-export default function Logo() {
-    return (
-        <div className={`${alata.className} font-bold text-4xl text-white leading-tight`}>loopstudios</div>
-    )
-}
